6f51f452fb8225473090a22fc6e9c3b6c47b3cf0
Shoaib Ahmed <153922877+shoaibsmit@users.noreply.github.com>
2024-09-08 15:59:44 +0500

    Add files via upload

---
+++ src/firebase/firbaseconfig.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAyTtP8vnsYsk3uNO8RSwgnfsr92hd-XNY",
+    authDomain: "next-js-c38e9.firebaseapp.com",
+    projectId: "next-js-c38e9",
+    storageBucket: "next-js-c38e9.appspot.com",
+    messagingSenderId: "481050342270",
+    appId: "1:481050342270:web:f292b1cf16f0d6f70c792f",
+    measurementId: "G-JJ5GH3JCDM"
+  };
+
+
+  const app = initializeApp(firebaseConfig);
+
